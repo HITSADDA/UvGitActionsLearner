c4dbc7087d2f0215b60bb2390af88366910a494b
vishal_singh_rajpurohit <gamedevelopmentuv@gmail.com>
2026-08-01 08:24:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express";
+
+const PORT = process.env.PORT
+
+const app = express()
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World! V1')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
